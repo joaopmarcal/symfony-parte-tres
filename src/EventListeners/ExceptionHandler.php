e324e3b545b766efc9ef9093d4fb83b37da98926
Joao Paulo <joaopaulozangrande@gmail.com>
2021-11-23 19:21:27 -0300

    generating error logs

--- src/EventListeners/ExceptionHandler.php
+++ src/EventListeners/ExceptionHandler.php
@@ -4,20 +4,26 @@ namespace App\EventListeners;
 
 use App\Entity\HypermidiaResponse;
 use App\Helper\EntityFactoryException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class ExceptionHandler implements EventSubscriberInterface
 {
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
     public static function getSubscribedEvents()
     {
         return [
             KernelEvents::EXCEPTION => [
                 ['handleEntityException', 1],
-                ['handle404Exception', 0]
+                ['handle404Exception', 0],
+                ['handleGenericException', -1]
             ],
         ];
     }
@@ -40,5 +46,15 @@ class ExceptionHandler implements EventSubscriberInterface
             $response->setStatusCode(Response::HTTP_BAD_REQUEST);
             $event->setResponse($response);
         }
+    }
+
+    public function handleGenericException(ExceptionEvent $event)
+    {
+        $this->logger->critical('Uma exceção ocorreu. {stack}',[
+            'stack' => $event->getException()->getTraceAsString()
+        ]);
+
+        $response = HypermidiaResponse::fromError($event->getException());
+        $event->setResponse($response->getResponse());
     }
 }
